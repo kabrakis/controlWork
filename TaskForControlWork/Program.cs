d82cbdf641bbef7b6df3dd2a25c726945adb5c4e
Ivan <zag091@mail.ru>
2023-04-27 15:46:44 +0300

    Create 2 array

--- TaskForControlWork/Program.cs
+++ TaskForControlWork/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string[] array1 = new string[] {"Hello", "2", "World", ":-)"};
+string[] array2 = new string[array1.Length];
+
