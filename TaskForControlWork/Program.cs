760f7de749225ec910f1eb91b19ea2b6c343e53a
Ivan <zag091@mail.ru>
2023-04-27 15:49:09 +0300

    Add Second  function

--- TaskForControlWork/Program.cs
+++ TaskForControlWork/Program.cs
@@ -2,7 +2,19 @@
 string[] array1 = new string[] {"Hello", "2", "World", ":-)"};
 string[] array2 = new string[array1.Length];
 
-
+//метод создания второго массива из элементов первого согласно условию
+void BuilderSecArrWhithElmLessThreeOnFistArr(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
 // метод печати массива
 void Print1DArray(string[] arr)
 {
@@ -13,4 +25,3 @@ void Print1DArray(string[] arr)
     }
     Console.WriteLine(arr[arr.Length - 1] + "]");
 }
-
