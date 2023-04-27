5eb73b9c234ce6cdc580cf52510056dcc15cf05f
Ivan <zag091@mail.ru>
2023-04-27 15:48:15 +0300

    Add 1-st function

--- TaskForControlWork/Program.cs
+++ TaskForControlWork/Program.cs
@@ -2,3 +2,15 @@
 string[] array1 = new string[] {"Hello", "2", "World", ":-)"};
 string[] array2 = new string[array1.Length];
 
+
+// метод печати массива
+void Print1DArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length-2; i++)
+    {
+        Console.Write(arr[i]+ ",");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
